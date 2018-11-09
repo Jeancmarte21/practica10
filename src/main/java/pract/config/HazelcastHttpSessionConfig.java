6469ce4289e40717d4b72fef9914fbe2da9c6282
Jean Marte <jeancmarte21@gmail.com>
2018-11-09 19:25:50 -0400

    up

---
+++ src/main/java/pract/config/HazelcastHttpSessionConfig.java
@@ -0,0 +1,27 @@
+package pract.config;/*
+package web2018.config;
+
+import com.hazelcast.config.*;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+*/
+/**
+ * Created by JavaDeveloperZone on 08-01-2018.
+ *//*
+
+@Configuration
+public class HazelcastHttpSessionConfig {
+    @Bean
+    public Config hazelCastConfig(){
+        Config config = new Config();
+        config.setInstanceName("hazelcast-instance")        // hazel case instance name
+                .addMapConfig(
+                        new MapConfig()                     // create map
+                                .setName("configuration")
+                                .setMaxSizeConfig(new MaxSizeConfig(200, MaxSizeConfig.MaxSizePolicy.FREE_HEAP_SIZE))
+                                .setEvictionPolicy(EvictionPolicy.LRU)
+                                .setTimeToLiveSeconds(-1));     // cache will be available until it will remove manually. less then 0 means never expired.
+        return config;
+    }
+}
+*/
